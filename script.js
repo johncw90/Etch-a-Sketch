abcf91132fba092d80af044e2f62ecc6ae9facc7
John <nhgg6bkhjb@privaterelay.appleid.com>
2022-06-15 23:18:49 -0500

    Add button with prompt

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const container = document.querySelector(".container");
+const button = document.querySelector(".grid-btn");
+
 for (let i = 0; i < 256; i++) {
   const box = document.createElement("div");
   container.appendChild(box);
   box.addEventListener("mouseover", () => {
     box.style.backgroundColor = "purple";
+  });
+  button.addEventListener("click", () => {
+    let userInput = prompt("What size grid?");
+    box.style.backgroundColor = "orange";
+    i = userInput;
+    i = userInput;
   });
 }
